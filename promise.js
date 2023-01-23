87a4b9ad4b3240226ff517af62d3c0300fd032f5
LeeHwaRang <snow2271@gmail.com>
2023-01-23 11:59:40 +0900

    async, await

--- promise.js
+++ promise.js
@@ -8,6 +8,20 @@ const addSum = (a, b) => {
     }, 3000);
   });
 };
-addSum(10, 20)
-  .then((sum) => console.log({ sum }))
-  .catch((error) => console.log({ error }));
+
+// addSum(10, 20)
+//   .then((sum1) => addSum(sum1, 3))
+//   .then((sum2) => console.log({ sum2 }))
+//   .catch((error) => console.log({ error }));
+
+const totalSum = async () => {
+  try {
+    const sum1 = await addSum(10, 10);
+    const sum2 = await addSum(sum1, 10);
+    console.log(sum2);
+  } catch (error) {
+    if (error) console.log(error);
+  }
+};
+
+totalSum();
