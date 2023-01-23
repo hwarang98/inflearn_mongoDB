9e60674a7992a759578367720a21d3d388afcdec
LeeHwaRang <snow2271@gmail.com>
2023-01-23 13:30:33 +0900

    user schema create

---
+++ src/models/User.js
@@ -0,0 +1,18 @@
+import { Schema, model } from 'mongoose';
+
+const UsetScheam = new Schema(
+  {
+    username: { type: String, require: true },
+    name: {
+      first: { type: String, require: true },
+      last: { type: String, require: true },
+    },
+    age: Number,
+    email: String,
+  },
+  { timestamps: true }
+);
+
+const User = model('user', UsetScheam);
+
+export default User;
