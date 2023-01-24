a46022d41762eec74773a555b5abf6f05a375767
LeeHwaRang <snow2271@gmail.com>
2023-01-25 07:53:37 +0900

    blog schema create

---
+++ src/models/Blog.js
@@ -0,0 +1,15 @@
+import { Schema, model, Types } from 'mongoose';
+
+const BlogScheam = new Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    isLive: { type: Boolean, required: true, default: false }, // 임시저장 false 면 임시저장, true면 유저한테 노출
+    user: { type: Types.ObjectId, required: true, ref: 'user' },
+  },
+  { timestamps: true }
+);
+
+const Blog = model('blog', BlogScheam);
+
+export default Blog;
