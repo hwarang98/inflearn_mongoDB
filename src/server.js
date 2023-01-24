af159b9ace8cec196eff3f10cf0d5be9b9520b77
LeeHwaRang <snow2271@gmail.com>
2023-01-24 15:45:58 +0900

    mongoDB D 학습

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ const MONGO_URL =
 
 const server = async () => {
   try {
+    mongoose.set('strictQuery', true);
     await mongoose.connect(MONGO_URL);
     console.log('MongoDB 연결 성공');
     app.use(Express.json());
@@ -19,6 +20,21 @@ const server = async () => {
       try {
         const users = await User.find({});
         return res.send({ users });
+      } catch (error) {
+        console.log(error);
+        return res.status(500).send({ error: error.message });
+      }
+    });
+
+    app.get('/user/:userId', async (req, res) => {
+      try {
+        const { userId } = req.params;
+        if (!mongoose.isValidObjectId(userId)) {
+          return res.status(400).send({ error: 'invaild userId' });
+        }
+
+        const user = await User.findOne({ _id: userId });
+        return res.send({ user });
       } catch (error) {
         console.log(error);
         return res.status(500).send({ error: error.message });
@@ -34,9 +50,45 @@ const server = async () => {
         if (!name || !name.first || !name.last) {
           return res.status(400).send({ err: '성 or 이름을 입력해주세요.' });
         }
+
         const user = new User(req.body);
         await user.save();
         return res.send({ user });
+      } catch (error) {
+        console.log(error);
+        return res.status(500).send({ error: error.message });
+      }
+    });
+
+    app.delete('/user/:userId', async (req, res) => {
+      try {
+        const { userId } = req.params;
+        if (!mongoose.isValidObjectId(userId)) {
+          return res.status(400).send({ error: 'invaild userId' });
+        }
+        const user = await User.findOneAndDelete({ userId });
+        return res.send({ user });
+      } catch (error) {
+        console.log(error);
+        return res.status(500).send({ error: error.message });
+      }
+    });
+
+    app.patch('/user/:userId', async (req, res) => {
+      try {
+        const { userId } = req.params;
+        const { age } = req.body;
+        if (!mongoose.isValidObjectId(userId) || typeof age === 'string') {
+          return res.status(400).send({ error: '입력값을 확인해 주세요.' });
+        }
+
+        console.log(typeof age);
+        const user = await User.findByIdAndUpdate(
+          userId,
+          { $set: { age } },
+          { new: true }
+        );
+        return res.send(user);
       } catch (error) {
         console.log(error);
         return res.status(500).send({ error: error.message });
