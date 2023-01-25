a0efdcc9720f306303a0db15dad12b4c9719b80b
LeeHwaRang <snow2271@gmail.com>
2023-01-25 13:24:59 +0900

    blog api get, post, put, patch 구현
    - get: params의 값을 받아 특정 게시글 조회
    - post: 게시글 생성
    - put: params의 값을 받아 특정 게시글 업데이트
    - patch: params의 값을 받아 특정 게시글의 일부 내용 업데이트

--- src/routers/blogRouter.js
+++ src/routers/blogRouter.js
@@ -6,6 +6,8 @@ const blogRouter = Router();
 
 blogRouter.get('/', async (req, res) => {
   try {
+    const blog = await Blog.find({});
+    return res.send({ blog });
   } catch (error) {
     console.log(error);
     res.status(500).send({ error: error.message });
@@ -14,6 +16,12 @@ blogRouter.get('/', async (req, res) => {
 
 blogRouter.get('/:blogId', async (req, res) => {
   try {
+    const { blogId } = req.params;
+    if (!isValidObjectId(blogId)) {
+      return res.status(400).send({ error: 'blogId를 확인해주세요.' });
+    }
+    const blog = await Blog.findById({ _id: blogId });
+    return res.send({ blog });
   } catch (error) {
     console.log(error);
     res.status(500).send({ error: error.message });
@@ -23,13 +31,18 @@ blogRouter.get('/:blogId', async (req, res) => {
 blogRouter.post('/', async (req, res) => {
   try {
     const { title, content, isLive, userId } = req.body;
-    if (typeof title !== 'string') res.status(400).send({ error: 'title을 입력해주세요.' });
-    if (typeof content !== 'string') res.status(400).send({ error: 'content를 입력해주세요.' });
-    if (isLive && typeof isLive !== 'string') res.status(400).send({ error: 'isLive는 boolean 입니다.' });
-    if (!isValidObjectId(userId)) res.status(400).send({ error: 'ID를 확인해주세요.' });
+    if (typeof title !== 'string')
+      res.status(400).send({ error: 'title을 입력해주세요.' });
+    if (typeof content !== 'string')
+      res.status(400).send({ error: 'content를 입력해주세요.' });
+    if (isLive && typeof isLive !== 'string')
+      res.status(400).send({ error: 'isLive는 boolean 입니다.' });
+    if (!isValidObjectId(userId))
+      res.status(400).send({ error: 'ID를 확인해주세요.' });
 
     const user = await User.findById(userId);
-    if (!user) res.status(400).send({ error: '해당 유저가 존재하지 않습니다.' });
+    if (!user)
+      res.status(400).send({ error: '해당 유저가 존재하지 않습니다.' });
 
     const blog = new Blog({ ...req.body, user });
     await blog.save();
@@ -40,8 +53,28 @@ blogRouter.post('/', async (req, res) => {
   }
 });
 
-blogRouter.put('/', async (req, res) => {
+blogRouter.put('/:blogId', async (req, res) => {
   try {
+    const { title, content } = req.body;
+    const { blogId } = req.params;
+    if (!isValidObjectId(blogId)) {
+      return res.status(400).send({ error: 'blogId를 확인해주세요.' });
+    }
+
+    if (typeof title !== 'string') {
+      res.status(400).send({ error: 'title을 입력해주세요.' });
+    }
+
+    if (typeof content !== 'string') {
+      res.status(400).send({ error: 'content를 입력해주세요.' });
+    }
+
+    const blog = await Blog.findByIdAndUpdate(
+      { _id: blogId },
+      { title, content },
+      { new: true }
+    );
+    return res.send(blog);
   } catch (error) {
     console.log(error);
     res.status(500).send({ error: error.message });
@@ -50,6 +83,24 @@ blogRouter.put('/', async (req, res) => {
 
 blogRouter.patch('/:blogId/live', async (req, res) => {
   try {
+    const { blogId } = req.params;
+    const { isLive } = req.body;
+
+    if (!isValidObjectId(blogId)) {
+      return res.status(400).send({ error: 'blogId를 확인해주세요.' });
+    }
+
+    if (typeof isLive !== 'boolean') {
+      return res.status(400).send({ error: 'isLive가 boolean값이 아닙니다' });
+    }
+
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      { isLive },
+      { new: true }
+    );
+
+    return res.send({ blog });
   } catch (error) {
     console.log(error);
     res.status(500).send({ error: error.message });
