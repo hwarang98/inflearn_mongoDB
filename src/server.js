18bf096592952de3da1ab4ee24bab0137db5edf6
LeeHwaRang <snow2271@gmail.com>
2023-02-05 21:02:13 +0900

    코드수정

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import Express from 'express';
 import mongoose from 'mongoose';
 import router from './routers/index.js';
-import generateFakeData from '../faker2.js';
+// import generateFakeData from '../faker2.js';
+import generateFakeData from '../faker.js';
 
 const app = Express();
 const port = 8080;
@@ -15,17 +16,14 @@ const server = async () => {
     // mongoose.set('debug', true);
     await mongoose.connect(MONGO_URL);
 
-    // 목업 데이터 db save
-
     console.log('MongoDB 연결 성공');
 
-    app.use(Express.json());
     app.use(router);
 
     app.listen(port, async () => {
       console.log(`server listening on port ${port}`);
 
-      // await generateFakeData(3, 10, 50);
+      // await generateFakeData(100000, 10, 50);
     });
   } catch (error) {
     throw error;
