d1a0912904846dd15cf928bfbf7931eeee3d9ec5
LeeHwaRang <snow2271@gmail.com>
2023-02-08 23:02:18 +0900

    트랙잭션 사용하지않고 아토믹하게 데이터 저장

--- src/controllers/comment.js
+++ src/controllers/comment.js
@@ -17,44 +17,54 @@ export default {
   },
 
   async postComment(req, res) {
-    const session = await startSession();
+    // const session = await startSession();
     let comment;
     try {
-      await session.withTransaction(async () => {
-        const { blogId } = req.params;
-        const { content, userId } = req.body;
-        if (!isValidObjectId(blogId)) return res.status(400).send({ error: 'blogId를 확인해주세요.' });
-        if (!isValidObjectId(userId)) return res.status(400).send({ error: 'userId 확인해주세요.' });
-        if (typeof content !== 'string') return res.status(400).send({ error: 'content를 확인해주세요.' });
+      // await session.withTransaction(async () => {
+      const { blogId } = req.params;
+      const { content, userId } = req.body;
+      if (!isValidObjectId(blogId)) return res.status(400).send({ error: 'blogId를 확인해주세요.' });
+      if (!isValidObjectId(userId)) return res.status(400).send({ error: 'userId 확인해주세요.' });
+      if (typeof content !== 'string') return res.status(400).send({ error: 'content를 확인해주세요.' });
 
-        const [blog, user] = await Promise.all([
-          BlogScheam.findById(blogId, {}, { session }), // find에도 꼭 session 을 넣을것!
-          UserScheam.findById(userId, {}, { session }),
-        ]);
+      const [blog, user] = await Promise.all([
+        BlogScheam.findById(blogId, {}, {}), // find에도 꼭 session 을 넣을것!
+        UserScheam.findById(userId, {}, {}),
+      ]);
 
-        if (!blog || !user) return res.status(400).send({ error: 'blog or user가 없습니다.' });
-        if (!blog.isLive) return res.status(400).send({ error: 'blog가 비공개 되어있습니다.' });
-        comment = new CommentScheam({
-          content,
-          user,
-          userFullName: `${user.name.first} ${user.name.last}`,
-          blog: blogId,
-        });
-
-        // await Promise.all([comment.save(), Blog.updateOne({ _id: blogId }, { $push: { comment: comment } })]);
-
-        blog.commentCount++;
-        blog.comment.push(comment);
-        if (blog.commentCount > 3) blog.comment.shift();
-
-        await Promise.all([comment.save({ session }), blog.save()]); // blog에는 find 할때 session을 넣어줬기때문에 내장되있어서 필요없음
+      if (!blog || !user) return res.status(400).send({ error: 'blog or user가 없습니다.' });
+      if (!blog.isLive) return res.status(400).send({ error: 'blog가 비공개 되어있습니다.' });
+      comment = new CommentScheam({
+        content,
+        user,
+        userFullName: `${user.name.first} ${user.name.last}`,
+        blog: blogId,
       });
+      // await session.abortTransaction();
+
+      // await Promise.all([comment.save(), Blog.updateOne({ _id: blogId }, { $push: { comment: comment } })]);
+
+      // blog.commentCount++;
+      // blog.comment.push(comment);
+      // if (blog.commentCount > 3) blog.comment.shift();
+
+      // blog에는 find 할때 session을 넣어줬기때문에 내장되있어서 필요없음
+      // await Promise.all([comment.save({}), blog.save()]);
+      // });
+
+      await Promise.all([
+        comment.save(),
+        BlogScheam.updateOne(
+          { _id: blogId },
+          { $inc: { commentCount: 1 }, $push: { comment: { $each: [comment], $slice: -3 } } }
+        ),
+      ]);
 
       return res.send({ comment });
     } catch (error) {
       return res.status(400).send({ error: error.message });
     } finally {
-      await session.endSession;
+      //   await session.endSession;
     }
   },
 
